fd999b6faf9a2324751a4267ec8558c63c056672
Atharva kulkarni <kulkarniatharva.s@gmail.com>
2022-10-26 14:59:13 -0400

    update in user

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -8,6 +8,77 @@ const { transactionCollection } = require("../data/Refs");
 //const { getDatabase } = require("firebase-admin/database");
 //const { ref, child, get } = require("firebase/database");
 
+// "isPassword" function used to check pass
+function isPassword(str) {
+  if (!str) throw `Password must be provided`;
+  if (typeof str != "string") throw `Password must be a string`;
+  if (str.trim().length == 0) throw `Password cannot just be empty spaces`;
+  if (str.toLowerCase().trim() != str.toLowerCase().trim().replace(/\s+/g, ""))
+    throw `Password cannot have spaces`;
+  let strippedStr = str.toLowerCase().trim().replace(/\s+/g, "");
+  if (strippedStr.length < 6) throw `Password must be at least six characters`;
+}
+
+// "SIGNUP" Router calling "CreateUser"
+router.post("/signup", async (req, res) => {
+  try {
+    //const { publicId, password, firstName, lastName, email, profilePhotoUrl, type } = req.body;
+    const { name, email, password, phone, children, wallet, userName } =
+      req.body;
+
+    isPassword(password);
+    const userData = {
+      name: name,
+      id: v4(),
+      phone: phone,
+      email: email,
+      children: children,
+      wallet: wallet,
+      lastActive: null,
+      isActive: false,
+    };
+    userCollection()
+      .orderByChild("id")
+      .equalTo(userData.id)
+      .once("value", (snapshot) => {
+        try {
+          let result = [];
+          userCollection().off("value");
+          for (var key in snapshot.val()) {
+            result.push({ id: key, ...snapshot.val()[key] });
+          }
+          if (result.length >= 1) {
+            res
+              .status(404)
+              .json(
+                "An account with this email already exists. Please try logging in."
+              );
+          } else {
+            userCollection(userData.id).set(userData, (error) => {
+              if (error) {
+                res
+                  .status(500)
+                  .json({ error: "User could not be registered!" });
+              } else {
+                res.json(userData);
+              }
+            });
+          }
+        } catch (error) {
+          console.log(error);
+          res
+            .status(500)
+            .json({ error: error.message ? error.messsage : error });
+          console.log(error);
+        }
+      });
+    // res.json('success');
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: error.message ? error.messsage : error });
+  }
+});
+
 // "searchByUserName" router calling "userCollection"
 router.get("/searchByUserName", async (req, res) => {
   try {
@@ -104,53 +175,6 @@ router.get("/logout/:userId", async (req, res) => {
   });
 });
 
-router.post("/invite/email", async (req, res) => {
-  try {
-    const { reciever, sender } = req.body;
-    if (!reciever || !sender) {
-      res.status(400).json({ error: "Insufficent inputs" });
-      return;
-    }
-
-    const output = `<div>
-      <p><strong>${sender.displayName}</strong> invites you to join <strong>CS 555</strong>.</p>
-      <a href='http://34.238.51.139:3000/'>Click here</a>
-    </div>`;
-
-    let transporter = nodemailer.createTransport({
-      service: "gmail",
-      auth: {
-        type: "OAuth2",
-        user: process.env.AUTH_USERNAME,
-        pass: process.env.AUTH_PASSWORD,
-        clientId: process.env.AUTH_CLIENT_ID,
-        clientSecret: process.env.AUTH_SECRET,
-        refreshToken: process.env.AUTH_REFRESH_TOKEN,
-      },
-    });
-
-    let info = await transporter.sendMail({
-      from: `akulka12@stevens.edu`, // sender address
-      to: reciever, // list of receivers
-      subject: "Invite to CS 555", // Subject line
-      text: `Hello, you've been invited to join CS 555 by ${sender.displayName}`, // plain text body
-      html: output, // html body
-    });
-
-    res.json({ messageId: info.messageId });
-    // userCollection(reciever).once('value', snapshot => {
-    //   if(!snapshot.val()) {
-    //     res.status(500).json({error: "Could not get reciever info"});
-    //     return;
-    //   }
-
-    // })
-  } catch (error) {
-    console.log(error);
-    res.status(500).json({ error: "Could not send email to the user" });
-  }
-});
-
 router.post("/requestMoney", async (req, res) => {
   const { sender_id, receiver_id, amount, state, tag } = req.body;
 
@@ -181,14 +205,13 @@ router.post("/requestMoney", async (req, res) => {
     });
 });
 
-router.post("/allocateMoney", async (req, res) => {
+router.post("/splitMoney", async (req, res) => {
   const { sender_id, receiver_ids, amount, timestamp, state, tag } = req.body;
   totalMoney = 0;
   noOfChildren = 0;
   let sender_name = "";
 
   const hash = new Map();
-  console.log(sender_id + "----");
 
   userCollection().on(
     "value",
@@ -197,9 +220,9 @@ router.post("/allocateMoney", async (req, res) => {
         hash.set(snapshot.val()[key]["name"], snapshot.val()[key]);
 
         if (key === sender_id) {
-            totalMoney = snapshot.val()[key]["wallet"];
-            noOfChildren = snapshot.val()[key]["children"].length;
-            sender_name = snapshot.val()[key]["name"];
+          totalMoney = snapshot.val()[key]["wallet"];
+          noOfChildren = snapshot.val()[key]["children"].length;
+          sender_name = snapshot.val()[key]["name"];
         }
       }
     },
@@ -259,8 +282,91 @@ router.post("/allocateMoney", async (req, res) => {
   }
 });
 
-router.get("/parent/pending/:id", async (req, res) => {
+router.post("/allocateMoney", async (req, res) => {
+  const { sender_id, childrenNames, amount, timestamp, state, tag } = req.body;
+  totalMoney = 0;
+  noOfChildren = 0;
+  let sender_name = "";
 
+  const hash = new Map();
+  try {
+    userCollection().on(
+      "value",
+      (snapshot) => {
+        for (let key in snapshot.val()) {
+          hash.set(snapshot.val()[key]["name"], snapshot.val()[key]);
+
+          if (key === sender_id) {
+            totalMoney = snapshot.val()[key]["wallet"];
+
+            noOfChildren = snapshot.val()[key]["children"].length;
+            if (totalMoney < amount * noOfChildren) {
+              res.status(500).send("Not Enough Money!");
+            }
+            sender_name = snapshot.val()[key]["name"];
+          }
+        }
+      },
+      (errorObject) => {
+        res.status(500).send(errorObject);
+      }
+    );
+
+    parentObj = hash.get(sender_name);
+    lstOfChildren = childrenNames;
+
+    for (let child in lstOfChildren) {
+      let obj = hash.get(lstOfChildren[child]);
+      obj["wallet"] += amount;
+      userCollection(obj["id"]).update(obj, (error) => {
+        if (error) {
+          res.status(500).json({ error: " " });
+        } else {
+          res.json(obj);
+        }
+      });
+    }
+    parentObj["wallet"] -= lstOfChildren.length * amount;
+    userCollection()
+      .orderByChild("id")
+      .equalTo(parentObj["id"])
+      .once("value", (snapshot) => {
+        userCollection(parentObj["id"]).update(parentObj, (error) => {
+          if (error) {
+            res.status(500).json({ error: " " });
+          } else {
+            res.json(parentObj);
+          }
+        });
+      });
+
+    //updating transaction table
+    for (let i in lstOfChildren) {
+      let child = lstOfChildren[i];
+      let childObj = hash.get(child);
+      let transactionData = {
+        sender_id: sender_id,
+        receiver_id: childObj["id"],
+        amount: each,
+        timestamp: new Date(),
+        state: "Done",
+        tag: tag,
+      };
+      console.log(sender_id);
+      transactionCollection(v4()).set(transactionData, (error) => {
+        if (error) {
+          res.status(500).json({ error: " " });
+        } else {
+          res.json(transactionData);
+        }
+      });
+    }
+  } catch (e) {
+    console.log(e);
+  }
+});
+
+router.get("/parent/pending/:id", async (req, res) => {
   const parentId = req.params.id;
   console.log(parentId);
   let result = [];
