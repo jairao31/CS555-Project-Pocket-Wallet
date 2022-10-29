f8c6d087705f825ef2584e302dda2fade827b077
Atharva kulkarni <kulkarniatharva.s@gmail.com>
2022-10-28 20:19:22 -0400

    update in firebase

--- backend/data/firebase.js
+++ backend/data/firebase.js
@@ -8,14 +8,13 @@ const { getStorage } = require("firebase/storage");
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBhCnBS7Q-YOVLT-YYqa6NbY2ZoHkS-Koc",
-  authDomain: "pocket-wallet-12969.firebaseapp.com",
-  databaseURL: "https://pocket-wallet-12969-default-rtdb.firebaseio.com",
-  projectId: "pocket-wallet-12969",
-  storageBucket: "pocket-wallet-12969.appspot.com",
-  messagingSenderId: "484931660463",
-  appId: "1:484931660463:web:8a2bad772c72a0f877425a",
-  measurementId: "G-GPTKVVVFFE"
+  apiKey: "AIzaSyAIAbck_qGrnqRv9X_obL-Qx9NMjUma2Hc",
+  authDomain: "temp-ac2ca.firebaseapp.com",
+  databaseURL: "https://temp-ac2ca-default-rtdb.firebaseio.com",
+  projectId: "temp-ac2ca",
+  storageBucket: "temp-ac2ca.appspot.com",
+  messagingSenderId: "354153288696",
+  appId: "1:354153288696:web:a35ab02a54371aaf6cfc0d"
 };
 
 // Initialize Firebase
