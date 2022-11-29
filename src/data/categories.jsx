7af5dbc6dec4b2e61144d0deecc63527ab7fbe2b
RitWickReddy <rritwik586@gmail.com>
2022-11-29 15:14:51 -0500

    re based data folder

---
+++ src/data/categories.jsx
@@ -0,0 +1,115 @@
+export const Blue = {
+  background: "#4285F4",
+};
+export const Green = {
+  background: "#0F9D58",
+};
+
+export const Red = {
+  background: "#DB4437",
+};
+
+export const Yellow = {
+  background: "#F4B400",
+};
+
+const categories = [
+  {
+    iconclass: "fa fa-money",
+    title: "Salary",
+    description: "Includes Official Salary Given by the Day Job you used to do",
+    style: Yellow,
+  },
+  {
+    iconclass: "fa fa-university",
+    title: "Bonus",
+    description:
+      "Any type of bonus at your day job Increment or any hike in your Paycheque",
+    style: Blue,
+  },
+  {
+    iconclass: "fa fa-inr",
+    title: "Investment Returns",
+    description: "Any type of Returns from the Stocks or Mutual Funds",
+    style: Yellow,
+  },
+  {
+    iconclass: "fa fa-credit-card",
+    title: "Bills",
+    description:
+      "Includes electricity bill, water bill, Gas bill and all other monthly bills ",
+    style: Blue,
+  },
+  {
+    iconclass: "fa fa-building",
+    title: "EMI",
+    description:
+      "Includes EMI of Home Car or anything on amazon whose premium needs to be paid ",
+    style: Red,
+  },
+  {
+    iconclass: "fa fa-youtube-play",
+    title: "Entertainment",
+    description:
+      "Includes movie tickets some expensive experience , Amusement park tickets fair ",
+    style: Green,
+  },
+  {
+    iconclass: "fa fa-cutlery",
+    title: "Food and Drinks",
+    description:
+      "Eating out in Restrarent Street Food Night Life and many such Foodie things",
+    style: Yellow,
+  },
+
+  {
+    iconclass: "fa fa-car",
+    title: "Fuel",
+    description: "Petrol Disel or Gasoline Pays for Cars",
+    style: Blue,
+  },
+
+  {
+    iconclass: "fa fa-shopping-cart",
+    title: "Groceries",
+    description:
+      "Dmart Shopping for Pulses , Spices or other cooking related things",
+    style: Red,
+  },
+  {
+    iconclass: "fa fa-heartbeat",
+    title: "Health",
+    description: "Pays to Doctors , Medical Shops ",
+    style: Green,
+  },
+  {
+    iconclass: "fa fa-line-chart",
+    title: "Investment",
+    description:
+      "Pay cut for Investment in Stocks, Fixed Deposit, Mutual Funds",
+    style: Yellow,
+  },
+
+  {
+    iconclass: "fa fa-cart-plus",
+    title: "Shopping",
+    description: "General Shopping at Malls or Stores",
+    style: Blue,
+  },
+
+  {
+    iconclass: "fa fa-taxi",
+    title: "Travel",
+    description:
+      "For Work or For Picnic all the pays to travelling from one place to the another",
+    style: Red,
+  },
+  {
+    iconclass: "fa fa-list",
+    title: "Other",
+    description: "All the other pays - Blah Blah Blah",
+    style: Green,
+  },
+];
+
+export default categories;
---
+++ src/data/userimages.jsx
@@ -0,0 +1,33 @@
+import u1 from "./../assets/img/user-avatars/user (1).png";
+import u2 from "./../assets/img/user-avatars/user (2).png";
+import u3 from "./../assets/img/user-avatars/user (3).png";
+
+import u4 from "./../assets/img/user-avatars/user (4).png";
+import u5 from "./../assets/img/user-avatars/user (5).png";
+import u6 from "./../assets/img/user-avatars/user (6).png";
+
+import u7 from "./../assets/img/user-avatars/user (7).png";
+import u8 from "./../assets/img/user-avatars/user (8).png";
+import u9 from "./../assets/img/user-avatars/user (9).png";
+
+export const user1 = u1;
+export const user2 = u2;
+export const user3 = u3;
+export const user4 = u4;
+export const user5 = u5;
+export const user6 = u6;
+export const user7 = u7;
+export const user8 = u8;
+export const user9 = u9;
+
+export const imgArr = [
+  user1,
+  user2,
+  user3,
+  user4,
+  user5,
+  user6,
+  user7,
+  user8,
+  user9,
+];
